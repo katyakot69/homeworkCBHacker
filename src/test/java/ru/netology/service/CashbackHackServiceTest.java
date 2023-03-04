98fea2ceca08861ba251d31af0d53882fdf64dad
Kate <ktorochkova@gmail.com>
2023-03-04 23:48:25 +0300

    add junit4 test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,43 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
-  
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCalculateIfAmountCorrect() {
+        int amount = 2600;
+        int actual = service.remain(amount);
+        int expected = 400;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmount1000() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountIncorrect() {
+        int amount = -2000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
 }
