122c1a03204c8edd7cb248373cb0fbd5ee36a115
Kate <ktorochkova@gmail.com>
2023-03-04 23:53:36 +0300

    add testng test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,4 +1,26 @@
-import static org.testng.Assert.*;
+package ru.netology.service;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
 public class CashbackHackServiceTest {
-    
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturnNull() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void shouldReturn100() {
+        int actual = service.remain(900);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+    }
+
+
 }
